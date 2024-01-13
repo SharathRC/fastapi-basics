574e2a58d55fe88a33d30b96d430a77c98216589
Sharath RC <sharathr.sh@gmail.com>
2024-01-13 18:28:31 +0100

    add fetch recipe api

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
 from typing import Union
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="Recipe API")
 
 
-@app.get("/")
+RECIPES = [
+    {
+        "id": 0,
+        "dish": "Chicken",
+        "source": "Web",
+        "url": "www.google.com",
+    },
+    {
+        "id": 2,
+        "dish": "Mutton",
+        "source": "Web",
+        "url": "www.google.com",
+    },
+    {
+        "id": 3,
+        "dish": "Duck",
+        "source": "Web",
+        "url": "www.google.com",
+    },
+    {
+        "id": 4,
+        "dish": "Goose",
+        "source": "Web",
+        "url": "www.google.com",
+    },
+]
+
+
+@app.get("/", status_code=200)
 async def read_root():
-    return {"msg": "hello world"}
+    return {"msg": "See recipes here"}
 
 
-@app.get("/items/{item_id}")
-async def read_items(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/recipes/{recipe_id}", status_code=200)
+async def fetch_recipe(recipe_id: int):
+    res = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    if res:
+        return res[0]
