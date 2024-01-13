dc66b43218a3a6ea2f139e17c4d0e8da2896b414
Sharath RC <sharathr.sh@gmail.com>
2024-01-13 18:40:32 +0100

    add search api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Optional
 from fastapi import FastAPI
 
 app = FastAPI(title="Recipe API")
@@ -7,25 +7,25 @@ app = FastAPI(title="Recipe API")
 RECIPES = [
     {
         "id": 0,
-        "dish": "Chicken",
+        "label": "Chicken",
         "source": "Web",
         "url": "www.google.com",
     },
     {
         "id": 2,
-        "dish": "Mutton",
+        "label": "Mutton",
         "source": "Web",
         "url": "www.google.com",
     },
     {
         "id": 3,
-        "dish": "Duck",
+        "label": "Duck",
         "source": "Web",
         "url": "www.google.com",
     },
     {
         "id": 4,
-        "dish": "Goose",
+        "label": "Goose",
         "source": "Web",
         "url": "www.google.com",
     },
@@ -33,12 +33,28 @@ RECIPES = [
 
 
 @app.get("/", status_code=200)
-async def read_root():
+async def read_root() -> dict:
     return {"msg": "See recipes here"}
 
 
 @app.get("/recipes/{recipe_id}", status_code=200)
-async def fetch_recipe(recipe_id: int):
+async def fetch_recipe(recipe_id: int) -> dict:
     res = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
     if res:
         return res[0]
+
+
+@app.get("/search/", status_code=200)
+async def search_recipes(
+    keyword: Optional[str] = None,
+    max_results: Optional[int] = 10,
+) -> dict:
+    if not keyword:
+        return {
+            "results": RECIPES[:max_results],
+        }
+
+    res = [recipe for recipe in RECIPES if keyword.lower() in recipe["label"].lower()]
+    return {
+        "results": res,
+    }
