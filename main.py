6b0c7ee33aaf88d5645fa6fcbd147b376b2c99f2
Sharath RC <sharathr.sh@gmail.com>
2024-01-13 17:54:52 +0100

    basic fastapi app

---
+++ main.py
@@ -0,0 +1,14 @@
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"msg": "hello world"}
+
+
+@app.get("/items/{item_id}")
+async def read_items(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
