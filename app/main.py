c55799d99beab1ba93124dce58aa6e28f9c4b9e9
Sharath RC <sharathr.sh@gmail.com>
2024-01-14 19:52:40 +0100

    update with pydantic model; add create recipe

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from typing import Union, Optional
 from fastapi import FastAPI
 
+from app.schemas import Recipe, RecipeCreate, RecipeSearchResults
+
 app = FastAPI(title="Recipe API")
 
 
@@ -9,25 +11,25 @@ RECIPES = [
         "id": 0,
         "label": "Chicken",
         "source": "Web",
-        "url": "www.google.com",
+        "url": "https://google.com",
     },
     {
         "id": 2,
         "label": "Mutton",
         "source": "Web",
-        "url": "www.google.com",
+        "url": "https://google.com",
     },
     {
         "id": 3,
         "label": "Duck",
         "source": "Web",
-        "url": "www.google.com",
+        "url": "https://google.com",
     },
     {
         "id": 4,
         "label": "Goose",
         "source": "Web",
-        "url": "www.google.com",
+        "url": "https://google.com",
     },
 ]
 
@@ -38,14 +40,15 @@ async def read_root() -> dict:
 
 
 @app.get("/recipes/{recipe_id}", status_code=200)
-async def fetch_recipe(recipe_id: int) -> dict:
+async def fetch_recipe(*, recipe_id: int) -> dict:
     res = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
     if res:
         return res[0]
 
 
-@app.get("/search/", status_code=200)
+@app.get("/search/", status_code=200, response_model=RecipeSearchResults)
 async def search_recipes(
+    *,
     keyword: Optional[str] = None,
     max_results: Optional[int] = 10,
 ) -> dict:
@@ -58,3 +61,22 @@ async def search_recipes(
     return {
         "results": res,
     }
+
+
+@app.post("/recipe/", status_code=201, response_model=Recipe)
+async def create_recipe(
+    *,
+    recipe_in: RecipeCreate,
+) -> dict:
+    new_recipe_id = len(RECIPES) + 1
+
+    new_recipe_entry = Recipe(
+        id=new_recipe_id,
+        label=recipe_in.label,
+        source=recipe_in.source,
+        url=recipe_in.url,
+    )
+
+    RECIPES.append(new_recipe_entry.model_dump())
+
+    return new_recipe_entry
