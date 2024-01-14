2d7a72ae4a216007cdc52757eb93394b90840dab
Sharath RC <sharathr.sh@gmail.com>
2024-01-14 21:16:37 +0100

    add recipe update and delete api

--- app/main.py
+++ app/main.py
@@ -14,19 +14,19 @@ RECIPES = [
         "url": "https://google.com",
     },
     {
-        "id": 2,
+        "id": 1,
         "label": "Mutton",
         "source": "Web",
         "url": "https://google.com",
     },
     {
-        "id": 3,
+        "id": 2,
         "label": "Duck",
         "source": "Web",
         "url": "https://google.com",
     },
     {
-        "id": 4,
+        "id": 3,
         "label": "Goose",
         "source": "Web",
         "url": "https://google.com",
@@ -35,7 +35,7 @@ RECIPES = [
 
 
 @app.get("/", status_code=200)
-async def read_root() -> dict:
+async def root() -> dict:
     return {"msg": "See recipes here"}
 
 
@@ -63,12 +63,12 @@ async def search_recipes(
     }
 
 
-@app.post("/recipe/", status_code=201, response_model=Recipe)
+@app.post("/recipes/", status_code=201, response_model=Recipe)
 async def create_recipe(
     *,
     recipe_in: RecipeCreate,
 ) -> dict:
-    new_recipe_id = len(RECIPES) + 1
+    new_recipe_id = len(RECIPES)
 
     new_recipe_entry = Recipe(
         id=new_recipe_id,
@@ -80,3 +80,34 @@ async def create_recipe(
     RECIPES.append(new_recipe_entry.model_dump())
 
     return new_recipe_entry
+
+
+@app.put("/recipes/{recipe_id}", status_code=200)
+async def update_recipe(
+    *,
+    recipe_id: int,
+    recipe_in: RecipeCreate,
+) -> dict:
+    res = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    if res:
+        recipe = res[0]
+        recipe["label"] = recipe_in.label
+        recipe["source"] = recipe_in.source
+        recipe["url"] = recipe_in.url
+
+        return Recipe.model_validate(recipe)
+    return {"msg": "Recipe not found"}
+
+
+@app.delete("/recipes/{recipe_id}", status_code=200)
+async def delete_recipe(
+    *,
+    recipe_id: int,
+) -> dict:
+    res = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    if res:
+        recipe = res[0]
+        RECIPES.remove(recipe)
+
+        return {"msg": "Deleted!"}
+    return {"msg": "Recipe not found"}
