e856a0879681f583357630449a972dfbd41940e2
Sharath RC <sharathr.sh@gmail.com>
2024-01-16 00:58:14 +0100

    refaxtor recipe

--- app/main.py
+++ app/main.py
@@ -10,38 +10,12 @@ from app.schemas import (
     RecipeUpdateRestricted,
 )
 
+from app.recipe_data import RECIPES
+
 BASE_PATH = Path(__file__).resolve().parent
 TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
 app = FastAPI(title="Recipe API")
-
-
-RECIPES = [
-    {
-        "id": 0,
-        "label": "Chicken",
-        "source": "Web",
-        "url": "https://google.com",
-    },
-    {
-        "id": 1,
-        "label": "Mutton",
-        "source": "Web",
-        "url": "https://google.com",
-    },
-    {
-        "id": 2,
-        "label": "Duck",
-        "source": "Web",
-        "url": "https://google.com",
-    },
-    {
-        "id": 3,
-        "label": "Goose",
-        "source": "Web",
-        "url": "https://google.com",
-    },
-]
 
 
 @app.get("/", status_code=200)
---
+++ app/recipe_data.py
@@ -0,0 +1,26 @@
+RECIPES = [
+    {
+        "id": 0,
+        "label": "Chicken",
+        "source": "Web",
+        "url": "https://google.com",
+    },
+    {
+        "id": 1,
+        "label": "Mutton",
+        "source": "Web",
+        "url": "https://google.com",
+    },
+    {
+        "id": 2,
+        "label": "Duck",
+        "source": "Web",
+        "url": "https://google.com",
+    },
+    {
+        "id": 3,
+        "label": "Goose",
+        "source": "Web",
+        "url": "https://google.com",
+    },
+]
