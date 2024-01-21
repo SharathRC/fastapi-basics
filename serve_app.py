5eacc7e7744d59e9d98150d8fa197a0bcbc18222
Sharath RC <sharathr.sh@gmail.com>
2024-01-21 21:34:09 +0100

    add serve app

---
+++ serve_app.py
@@ -0,0 +1,7 @@
+from app.main import app
+
+if __name__ == "__main__":
+    # Use this for debugging purposes only
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
